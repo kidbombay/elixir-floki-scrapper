bf51bb2467457a106f7fec8614e3dcb784cb5b2b
zaali <zaali@live.com>
2019-05-21 10:14:44 +0400

    Add title in tests

--- test/job_scrapper_test.exs
+++ test/job_scrapper_test.exs
@@ -10,14 +10,17 @@ defmodule JobScrapperTest do
       |> List.first
     {_, _, [company]} = List.first(info_childrens)
     {_, _, [location]} = List.last(info_childrens)
+    title = Floki.find(body, ".jobsearch-JobComponent .jobsearch-DesktopStickyContainer h3.jobsearch-JobInfoHeader-title") |> Floki.text()
 
     resp = JobScrapper.indeed_detail
     assert resp.company == company
     assert resp.location == location
+    assert resp.title == title
   end
 
   test "check JobScrapper.indeed_detail with static values" do
     resp = JobScrapper.indeed_detail
+    assert resp.title == "Rail Systems Resident Engineer"
     assert resp.company == "EPC Consultants, Inc."
     assert resp.location == "San Francisco, CA 94104"
   end

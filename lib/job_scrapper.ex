13937879ff922af8feece3a84be7c31c4893aa0e
zaali <zaali@live.com>
2019-05-21 09:48:22 +0400

    Implement indeed scrapper

--- lib/job_scrapper.ex
+++ lib/job_scrapper.ex
@@ -14,4 +14,27 @@ defmodule JobScrapper do
         IO.inspect reason
     end
   end
+
+   def zaali_detail() do
+    case HTTPoison.get(
+           "https://www.indeed.com/viewjob?jk=fb0cb865bc60f2b7&from=cobra&tk=1dbccvrnn1sav000&iaal=1"
+         ) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {_, _, info_childrens} = Floki.find(body, ".jobsearch-JobComponent .jobsearch-JobInfoHeader-subtitle .jobsearch-InlineCompanyRating")
+        |> List.first
+
+        {_, _, [company]} = List.first(info_childrens)
+        {_, _, [location]} = List.last(info_childrens)
+
+        %{
+          title: Floki.find(body, ".jobsearch-JobComponent .jobsearch-DesktopStickyContainer h3.jobsearch-JobInfoHeader-title") |> Floki.text(),
+          company: company,
+          location: location,
+          description: Floki.find(body, ".jobsearch-JobComponent .jobsearch-JobComponent-description") |> Floki.text(),
+        }
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
+
 end
